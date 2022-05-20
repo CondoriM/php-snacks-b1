de46cbba133887078dc81baeb56760b1ee17dde4
CondoriM <condoria.miguel@gmail.com>
2022-05-20 12:43:46 +0200

    created data

---
+++ snack1/index.php
@@ -0,0 +1,33 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+<?php
+
+$array = [
+    [
+        'casa' => 'yankees',
+        'punticasa' => '10',
+        'ospiti' => 'baltimora',
+        'puntiospiti' => '7',
+    ],
+    [
+        'casa' => 'cleverland',
+        'punticasa' => '9',
+        'ospiti' => 'houston',
+        'puntiospiti' => '5',
+    ],
+    [
+        'casa' => 'yankees',
+        'punticasa' => '5',
+        'ospiti' => 'totonto',
+        'puntiospiti' => '8',
+    ],
+    [
+        'casa' => 'houston',
+        'punticasa' => '9',
+        'ospiti' => 'baltimora',
+        'puntiospiti' => '7',
+    ]
+]
+
+?>
